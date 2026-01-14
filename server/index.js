b69c6b89100092ee7f6c08c27ee858d6e1277c77
Shepon Hossain <165515839+sheponhossain@users.noreply.github.com>
2026-01-15 01:16:07 +0600

    Refactor CORS configuration for improved clarity

--- server/index.js
+++ server/index.js
@@ -8,25 +8,22 @@ const app = express();
 
 connectDB();
 
-const allowedOrigins = [
-  'https://book-worm-front-end-orpin.vercel.app',
-  'http://localhost:3000'
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+const corsOptions = {
+  origin: 'https://book-worm-front-end-orpin.vercel.app',
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'user-role']
-}));
+};
 
-app.options('*', cors());
+app.use(cors(corsOptions));
+
+app.options('*', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://book-worm-front-end-orpin.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, user-role');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.sendStatus(200);
+});
 app.use(express.json());
 
 const Book = require('./models/Book');
